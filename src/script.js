631f6b92ed959b32ed691ac8addb366d9aa42fea
lreuter2020 <lreuter2020@gmail.com>
2023-04-26 14:53:47 -0700

    removed poems.json so it will work on mobile

--- src/script.js
+++ src/script.js
@@ -1,5 +1,12 @@
-import poems from "./poems.json" assert {type: "json" };
-console.log(poems)
+const poems = [
+    "It's ironic——\nwhen I wrap my arms around her,\nI'm the one that feels protected",
+    "Even\nif the day we met\nwould have never taken place,\nsomehow,\nsomeway,\nin another day,\nwe would have met\nand fallen in love\nlike the first day.",
+    "Her dress\nwas the least\nof her outfit——\nno one\nwore a smile\nlike her.",
+    "The light of the moon\nfailed in comparison\nto the light\nof her soul.",
+    "If I were a poet,\nI would write a book,\nfilled with poems and stories\nof the love\nyou have made me feel.",
+    "If one could\ncapture the\nbrightness of all the stars\nin the night sky\nyour smile would outshine it.",
+    "They say\n\"when a person falls in love\nthey feel as though \nthey have discovered \nlove for the first time\"\nNobody has ever felt\nthe way I feel\nabout you."
+]
 
 const INTERVAL_LENGTH = 7500;
 
@@ -20,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const content = document.getElementById("content");
     const text = document.getElementById("text");
     
-    setInterval(() => {
+    window.setInterval(() => {
         text.classList.add("fade");
         text.innerText = random(poems);
     
